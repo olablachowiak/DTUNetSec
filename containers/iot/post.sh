657cb81a43cfc51c5dd5a490e84186eb80f9c777
RicYaben <rmyl@dtu.dk>
2025-03-09 10:08:31 +0100

    add post to the supervisord

--- containers/iot/post.sh
+++ containers/iot/post.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+
+# Sleep for a while before things get started
+sleep 3
+
+# Register a printer
 lpadmin -p MyPrinter -E -v file:///tmp/fake_printer_output -m raw
 lpoptions -d MyPrinter
+echo "Test Print Job" | lp -d MyPrinter -t "secret"
